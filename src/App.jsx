eea6fb4b910f70662483f9bed3d651b4bf1d0323
Md Nurnabi Rana <mdnurnabirana.cse@gmail.com>
2025-09-22 00:59:16 +0600

    Add navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-
   return (
     <>
-      
+      <Navbar></Navbar>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import navImg from "../../assets/logo.png";
+import curr from "../../assets/currencyIcon.png";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar max-w-[1296px] mx-auto">
+        <div className="flex-1">
+          <a className="text-xl">
+            <img className="w-[60px] h-[60px]" src={navImg} alt="Logo" />
+          </a>
+        </div>
+        <div className="flex items-center">
+          <span className="mr-1">6000000</span>
+          <span className="mr-1">Coin</span>
+          <img src={curr} alt="currency Icon" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
