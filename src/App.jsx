819e958dc90918986d01de8d261d31ea55bfff80
Md Nurnabi Rana <mdnurnabirana.cse@gmail.com>
2025-09-22 21:44:25 +0600

    Add toggling functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import AvailablePlayers from "./components/AvailablePlayers/AvailablePlayers";
 import SelectedPlayers from "./components/SelectedPlayers/SelectedPlayers";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 
 // Player Api Fetching
 const fetchPlayers = async () => {
@@ -11,21 +11,44 @@ const fetchPlayers = async () => {
 };
 
 function App() {
+  const [toggle, setToggle] = useState(true);
   const playerPromise = fetchPlayers();
+
   return (
     <>
       {/* Navbar  */}
       <Navbar></Navbar>
+      <div className="max-w-[1296px] mx-auto flex justify-between items-center my-5">
+        <h1 className="font-bold text-2xl">Available Players</h1>
+        <div className="font-bold">
+          <button
+            onClick={() => setToggle(!toggle)}
+            className="btn rounded-l-2xl py-2 px-5 border-r-0 bg-[#E7FE29]"
+          >
+            Available Players
+          </button>
+          <button
+            onClick={() => setToggle(!toggle)}
+            className="btn rounded-r-2xl py-2 px-5 border-l-0"
+          >
+            Selected Players <span>(0)</span>
+          </button>
+        </div>
+      </div>
 
-      {/* Available Player Container Component */}
-      <Suspense fallback={
-        <span className="loading loading-dots loading-xl"></span>
-      }>
-        <AvailablePlayers playerPromise={playerPromise}></AvailablePlayers>
-      </Suspense>
-
-      {/* Selected Players Container */}
-      <SelectedPlayers></SelectedPlayers>
+      {toggle ? (
+        <Suspense
+          fallback={
+            <div className="flex justify-center">
+              <span className="loading loading-dots loading-xl"></span>
+            </div>
+          }
+        >
+          <AvailablePlayers playerPromise={playerPromise}></AvailablePlayers>
+        </Suspense>
+      ) : (
+        <SelectedPlayers></SelectedPlayers>
+      )}
     </>
   );
 }
