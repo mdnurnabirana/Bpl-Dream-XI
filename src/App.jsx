1f4d5144f2de7d9e227635793b5e3629e1306859
Md Nurnabi Rana <mdnurnabirana.cse@gmail.com>
2025-09-22 19:43:12 +0600

    Add Available Players Container

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import AvailablePlayers from "./components/AvailablePlayers/AvailablePlayers";
+import SelectedPlayers from "./components/SelectedPlayers/SelectedPlayers";
+import { Suspense } from "react";
+
+// Player Api Fetching
+const fetchPlayers = async () => {
+  const res = await fetch("/player.json");
+  return res.json();
+};
 
 function App() {
+  const playerPromise = fetchPlayers();
   return (
     <>
+      {/* Navbar  */}
       <Navbar></Navbar>
+
+      {/* Available Player Container Component */}
+      <Suspense fallback={
+        <span className="loading loading-dots loading-xl"></span>
+      }>
+        <AvailablePlayers playerPromise={playerPromise}></AvailablePlayers>
+      </Suspense>
+
+      {/* Selected Players Container */}
+      <SelectedPlayers></SelectedPlayers>
     </>
   );
 }
