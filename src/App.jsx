334fc79431cca9c668b10230fadb09044e035704
Md Nurnabi Rana <mdnurnabirana.cse@gmail.com>
2025-09-23 23:20:12 +0600

    Add responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { ToastContainer } from "react-toastify";
 import Banner from "./components/Banner/Banner";
 import Newsletter from "./components/Newsletter/Newsletter";
 import Footer from "./components/Footer/Footer";
+
 // Player Api Fetching
 const fetchPlayers = async () => {
   const res = await fetch("/player.json");
@@ -21,7 +22,7 @@ function App() {
 
   const removePlayer = (param) => {
     const filteredData = myPlayers.filter(ply => ply.id !== param.id);
-    setMyPlayers (filteredData);
+    setMyPlayers(filteredData);
     setAvailableBalance(availableBalance + param.price);
   }
 
@@ -30,24 +31,24 @@ function App() {
       {/* Navbar  */}
       <Navbar availableBalance={availableBalance}></Navbar>
       <Banner availableBalance={availableBalance} setAvailableBalance={setAvailableBalance}></Banner>
-      <div className="max-w-[1296px] mx-auto flex justify-between items-center my-5">
-        <h1 className="font-bold text-2xl">{
-          toggle ? "Available Players" : `Selected Players (${myPlayers.length}/6)`
-        }</h1>
+      <div className="max-w-[1296px] mx-auto flex flex-col md:flex-row justify-between items-center my-5 px-4">
+        <h1 className="font-bold text-2xl mb-4 md:mb-0">
+          {toggle ? "Available Players" : `Selected Players (${myPlayers.length}/6)`}
+        </h1>
         <div className="font-bold">
           <button
             onClick={() => setToggle(!toggle)}
             className={`btn rounded-l-2xl py-2 px-5 border-r-0 ${
-              toggle ? "bg-[#E7FE29]" : " "
-            } `}
+              toggle ? "bg-[#E7FE29]" : ""
+            }`}
           >
             Available Players
           </button>
           <button
             onClick={() => setToggle(!toggle)}
             className={`btn rounded-r-2xl py-2 px-5 border-r-0 ${
-              !toggle ? "bg-[#E7FE29]" : " "
-            } `}
+              !toggle ? "bg-[#E7FE29]" : ""
+            }`}
           >
             Selected Players <span>({myPlayers.length})</span>
           </button>
@@ -71,7 +72,7 @@ function App() {
           ></AvailablePlayers>
         </Suspense>
       ) : (
-        <SelectedPlayers removePlayer = {removePlayer} myPlayers={myPlayers} toggle={toggle} setToggle={setToggle}></SelectedPlayers>
+        <SelectedPlayers removePlayer={removePlayer} myPlayers={myPlayers} toggle={toggle} setToggle={setToggle}></SelectedPlayers>
       )}
 
       <Newsletter></Newsletter>
